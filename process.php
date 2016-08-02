093e36f3acd0a2d421e122592413fb8a80518053
Joe Christiansen <christiansenjoe123@outlook.com>
2016-08-02 15:59:54 +0000

    fixed the nav links

--- process.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include'db.php';
-    $name=$_POST['Name'];
-    $num=$_POST['Number'];
-    $n=new db();
-    $n->connect();
-    $n->insert($name,$num);
-?>
---
+++ recipe.sql
@@ -0,0 +1,8 @@
+create table recipes
+(
+    id int,
+    name varchar,
+    ingredients varchar
+);
+
+insert into recipes
